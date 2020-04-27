eb0ae0ac1559407cddbd99b7ae98242f74374e7e
Abdul-21 <53537785+Abdul-21@users.noreply.github.com>
2020-04-27 15:48:57 -0500

    Updated file directory for tomcat

--- WEB-INF/classes/bankapp.java
+++ WEB-INF/classes/bankapp.java
@@ -5,23 +5,26 @@ import javax.servlet.http.*;
 
 
 
-
-
-
 public class bankapp extends HttpServlet {
+  private static final long serialVersionUID = 102831973239L;
+
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
   }
   public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException,ServletException
 	{
+    PrintWriter out =response.getWriter();
     try{
       String login = request.getParameter("username");
     }catch(NumberFormatException nfEx){
+          out.println("<HTML>");
+          out.println("<HEAD>");
+          out.println("<TITLE>RIP</TITLE>");
+          out.println("</HEAD>");
     			// sendPage(response,"*** Invalid entry! ***");
     			return;
     }
-    PrintWriter out =response.getWriter();
     HttpSession userSession = request.getSession();
     if(userSession == null){
       out.println("<HTML>");
