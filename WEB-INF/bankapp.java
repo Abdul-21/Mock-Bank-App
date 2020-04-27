ece2f50460fd35354fc7aa5e5b1910e76f9d7745
Abdul-21 <53537785+Abdul-21@users.noreply.github.com>
2020-04-27 15:09:28 -0500

    Update bankapp and web.xml

--- WEB-INF/bankapp.java
+++ WEB-INF/bankapp.java
@@ -3,28 +3,36 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+
+
+
+
+
 public class bankapp extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
   public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException,ServletException
 	{
     try{
       String login = request.getParameter("username");
     }catch(NumberFormatException nfEx){
-    			sendPage(response,"*** Invalid entry! ***");
+    			// sendPage(response,"*** Invalid entry! ***");
     			return;
     }
+    PrintWriter out =response.getWriter();
     HttpSession userSession = request.getSession();
-    if(userSession == NULL){
-      PrintWriter out = reply.getWriter();
+    if(userSession == null){
       out.println("<HTML>");
       out.println("<HEAD>");
       out.println("<TITLE>User Session wasn't found</TITLE>");
       out.println("</HEAD>");
-    }
-    PrintWriter out = reply.getWriter();
+    }else{
     out.println("<HTML>");
     out.println("<HEAD>");
     out.println("<TITLE>Session was found</TITLE>");
     out.println("</HEAD>");
+  }
   }
 }
