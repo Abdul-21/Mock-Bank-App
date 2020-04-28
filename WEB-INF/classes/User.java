a632e0518344bf76198e5536f6ca24aefb4cc78b
Abdul-21 <53537785+Abdul-21@users.noreply.github.com>
2020-04-28 15:00:16 -0500

    New User class
    for user info to get the login the bank working

---
+++ WEB-INF/classes/User.java
@@ -0,0 +1,47 @@
+public class Client implements Serializable{
+
+private static final long serialVersionUID = -2284079786212849611L;
+private String firstName;
+private String lastName;
+private String accountID;
+private String accountType;
+private String password;
+
+public Client(){
+
+}
+
+public void setFirstName(String firstN) {
+	firstName = firstN;
+}
+public void setLastName(String lastN) {
+	lastName = lastN;
+}
+public void setacccountID(String id) {
+	accountID = id;
+}
+public void setacctType(String type) {
+	accountType = type;
+}
+public void setpassWord(String Password) {
+	this.Password = Password;
+}
+
+public String getFirstName() {
+	return firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public String getacctID() {
+	return accountID;
+}
+public String getacctType() {
+	return accountType;
+}
+public String getPassword() {
+	return Password;
+}
+
+
+}
